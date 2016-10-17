6a8d191a0031d842b372a5e59212dd8ac816e751
Jerry Orta <jerryorta.dev@gmail.com>
2016-10-17 04:20:31 -0500

    fix(build): format to amd
    close#7

--- tools/tasks/prod/build.js.prod.ts
+++ tools/tasks/prod/build.js.prod.ts
@@ -4,7 +4,7 @@ import { PROJECT_ROOT_APP_SRC,} from '../../config';
 export = (done: any) => {
   const outputOptions = {
     // amd, cjs or es6
-    format: 'umd',
+    format: 'amd',
     sourceMaps: true,
     inlineConditions: true,
     config: {
