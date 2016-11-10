93092233fff6701c414f70b569d5360cf0115731
Michael Fry-White <michaelfry2002@gmail.com>
2016-11-10 19:01:13 +0000

    Satisfy my OCD. Clean up some stuff not used in real app

--- src/browser/app/app.component.ts
+++ src/browser/app/app.component.ts
@@ -1,38 +1,15 @@
-import {  Component  } from '@angular/core';
+import { Component } from '@angular/core';
 import template from './app.component.html!text';
 import styles from './app.component.css!text';
 
-/**
- * Conditional import based on ~production flag in jspm.config:
- *
- *  "map": {
- *      "./config/env/prod.config": {
- *        "~production": "./config/env/dev.config"
- *      }
- *    }
- *
- *   gulp dev task will load ./config/env/dev.config.
- *   bulp prod task will load ./config/env/prod.config
- *
- *   This is a feature driven by SystemJS, where the SystemJS
- *   builder ( via JSPM ) builder.buildStatic method will
- *   load the "./config/env/prod.config" path.
- *
- *   For more info, see http://jspm.io/0.17-beta-guide/conditional-loading.html
- */
 import { Config } from './config/env/prod.config';
 
-/**
- * This class represents the main application component. Within the @Routes annotation is the configuration of the
- * applications routes, configuring the paths for the lazy loaded components (HomeComponent, AboutComponent).
- */
 @Component({
   moduleId: module.id,
-  selector: 'sd-app',
+  selector: 'app',
   template: template,
   styles: [styles]
 })
-
 export class AppComponent {
   constructor() {
     console.log('Environment config', Config);
--- src/browser/app/app.module.ts
+++ src/browser/app/app.module.ts
@@ -1,40 +1,17 @@
-import {  NgModule  } from '@angular/core';
-import {  BrowserModule  } from '@angular/platform-browser';
-import {  APP_BASE_HREF  } from '@angular/common';
-import {  RouterModule  } from '@angular/router';
-import {  HttpModule  } from '@angular/http';
-import {  AppComponent  } from './app.component';
-import {  routes  } from './app.routes';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
 
-import {  AboutModule  } from './_samples/about/about.module';
-import {  HomeModule  } from './_samples/home/home.module';
-import {  CacheBusterModule  } from './_samples/cacheBuster/cacheBuster.module';
-import {  SharedModule  } from './_samples/shared/shared.module';
-import {
-  FeatureModule
- } from './_samples/conditionalSubstitution/#{FEATURE|FeatureConfig.FEATURE_NAME}/feature.module';
+import { AboutModule } from './_samples/about/about.module';
+import { HomeModule } from './_samples/home/home.module';
+import { CacheBusterModule } from './_samples/cacheBuster/cacheBuster.module';
+import { SharedModule } from './_samples/shared/shared.module';
+import { FeatureModule } from './_samples/conditionalSubstitution/#{FEATURE|FeatureConfig.FEATURE_NAME}/feature.module';
 
-/**
- * Conditional import based on ~production flag in jspm.config:
- *
- *  "map": {
- *      "./config/env/prod.config": {
- *        "~production": "./config/env/dev.config"
- *      }
- *    }
- *
- *   gulp dev task will load ./config/env/dev.config.
- *   gulp prod task will load ./config/env/prod.config
- *
- *   This is a feature driven by SystemJS, where the SystemJS
- *   builder ( via JSPM ) builder.buildStatic method will
- *   load the "./config/env/prod.config" path.
- *
- *   ~production means NOT production. The production flag is set
- *   to true by the SystemJS builder internally.
- *
- *   For more info, see http://jspm.io/0.17-beta-guide/conditional-loading.html
- */
 import { Config } from './config/env/prod.config';
 
 var importModules = [
@@ -60,7 +37,5 @@ if (FeatureModule) {
     useValue: Config.BASE
   }],
   bootstrap: [AppComponent]
-
 })
-
 export class AppModule { }
